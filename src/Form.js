792ea304dbb5484fcf0992df86d5cf53c4993e8d
Madeleine Daphne Randall <madeleinedaphnerandall@Madeleines-MacBook-Air.local>
2022-07-06 17:52:11 +0100

    Initial set-up

---
+++ src/Form.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+export default function Form() {
+  // const [givenName, setGivenName] = useState("");
+  // const [surname, setSurname] = useState("");
+
+  return <form></form>;
+}

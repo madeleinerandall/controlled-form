963a3bb1e8277898f9a269cd34a5062096db7324
Madeleine Daphne Randall <madeleinedaphnerandall@Madeleines-MacBook-Air.local>
2022-07-06 19:04:53 +0100

    Add the sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+
+export default function Sidebar() {
+  return (
+    <div id="Sidebar">
+      <MailOutlineIcon />
+      <i>Please descibe your product idea in a nutshell</i>
+      <i>We need your email to reach you</i>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Form from "./Form";
+import Sidebar from "./Sidebar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Form />
+    <div id="app">
+      <Sidebar />
+      <Form />
+    </div>
   </React.StrictMode>
 );

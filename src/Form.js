8d55ab253ddeccd2262b872a646a7b2bc3a0b0e0
Madeleine Daphne Randall <madeleinedaphnerandall@Madeleines-MacBook-Air.local>
2022-07-06 18:35:44 +0100

    Add basic input fields

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,78 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function Form() {
-  // const [givenName, setGivenName] = useState("");
-  // const [surname, setSurname] = useState("");
+  const [shouldSucceed, setShouldSucceed] = useState(true);
+  const [error, setError] = useState("");
 
-  return <form></form>;
+  return (
+    <form>
+      <h1>Hire me</h1>
+      <TextField
+        label="Message"
+        type="text"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        multiline
+        rows={4}
+      />
+      <TextField
+        className="half"
+        label="Your Name"
+        type="name"
+        variant="outlined"
+        margin="normal"
+        required
+      />
+      <TextField
+        className="half"
+        label="Your Email"
+        type="email"
+        variant="outlined"
+        margin="normal"
+        required
+      />
+
+      <FormControlLabel
+        control={
+          <Switch
+            defaultChecked
+            checked={shouldSucceed}
+            onChange={() => setShouldSucceed(!shouldSucceed)}
+          />
+        }
+        label="Should Succeed"
+      />
+
+      {shouldSucceed ? null : (
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={error}
+            label="Age"
+            onChange={(e) => setError(e.target.value)}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      )}
+
+      <Button type="submit" margin="normal">
+        Submit
+      </Button>
+    </form>
+  );
 }
